3badbe01da2398e329182d62c97b5c789e6237d5
Admir Begovic <caketrainx@hotmail.com>
2014-02-05 08:35:13 -0100

    Hei, lagt til filene

---
+++ Scripts/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for a in 1 2 3; do
+        for b in a b c; do
+            echo "$a $b $c"
+        done
+      done
+
--- python/exercises/ex1.py
+++ python/exercises/ex1.py
@@ -2,6 +2,6 @@ print "Hello World"
 print "Hello again"
 print "I like typing this"
 print "This is fun"
-print "Yay! Printing"
+print "ay! Printing"
 print "I'd much rather you 'no'"
 print 'I "said" do nor rouch this.'
